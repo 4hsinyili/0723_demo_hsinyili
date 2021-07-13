b749ebae62900dc97e634be3273e5a3f38459d93
4hsinyili <4hsinyili@gmail.com>
2021-07-13 21:01:13 +0800

    Fix Bug: seleniumwire will get caught, so change back to selenium for a try

--- QS_Lambdas/qs_crawl_topic/src/QS_Modules/utils.py
+++ QS_Lambdas/qs_crawl_topic/src/QS_Modules/utils.py
@@ -1,4 +1,4 @@
-from seleniumwire import webdriver
+from selenium import webdriver
 import os
 
 
@@ -69,6 +69,9 @@ class Chrome():
             for argument in lambda_options:
                 options.add_argument(argument)
 
+            options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            options.add_experimental_option('useAutomationExtension', False)
+            options.add_argument("--disable-blink-features=AutomationControlled")
             user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
             options.add_argument(f'user-agent={user_agent}')
             options.binary_location = os.getcwd() + "/bin/headless-chromium"
